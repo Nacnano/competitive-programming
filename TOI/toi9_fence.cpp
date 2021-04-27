ef97800a08c16a522cd3f7795612d057ba76b2a3
Nacnano <59221274+Nacnano@users.noreply.github.com>
2021-04-27 22:44:14 +0700

    Add files via upload

--- TOI/toi9_fence.cpp
+++ TOI/toi9_fence.cpp
@@ -1,49 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int tree[510][510];
-int qsum[510][510];
-
-int sqsum(int a,int b,int x,int y)
-{
-	return qsum[y][x]-qsum[y][a-1]-qsum[b-1][x]+qsum[b-1][a-1];
-}
+int qs[505][505];
 
 int main(){
-	
-	int q=2;
-	
-	while(q--)
-	{
-		int m,n,t;
-		scanf("%d %d",&m,&n);
-		scanf("%d",&t);
-		while(t--)
-		{
-			int r,c;
-			scanf("%d %d",&r,&c);
-			tree[r+1][c+1]=1;
-		}
-		
-		for(int i=1;i<=m;i++) for(int j=1;j<=n;j++) qsum[i][j]=qsum[i-1][j]+qsum[i][j-1]-qsum[i-1][j-1]+tree[i][j];
-	
-		int ans=-1;
-		for(int i=1;i<=m;i++)
-		{
-			for(int j=1;j<=n;j++)
-			{
-				int mn=min(m-i+1,n-j+1);
-				for(int k=0;k<=mn;k++)
-				{
-					if(i+k>m || j+k>n) break;
-					
-					if(sqsum(j,i,j+k,i+k)-sqsum(j+1,i+1,j+k-1,i+k-1)==0)	
-						ans=max(ans,k);
-				}
-			}
-		}	
-		printf("%d\n",ans+1);
-		
-		for(int i=0;i<=m;i++) for(int j=0;j<=n;j++) tree[i][j]=0,qsum[i][j]=0;
-	}
+
+    int q=2;
+    while(q--)
+    {
+        int n,m;
+        scanf("%d%d",&n,&m);
+
+        int t;
+        scanf("%d",&t);
+        while(t--)
+        {
+            int x,y;
+            scanf("%d%d",&x,&y);
+            qs[x+1][y+1]=1;
+        }
+
+        for(int i=1;i<=n;i++) for(int j=1;j<=m;j++) qs[i][j]+=qs[i-1][j]+qs[i][j-1]-qs[i-1][j-1];
+
+        int yes=1;
+        for(int k=min(n,m);k>=0 && yes;k--)
+            for(int i=0;i+k<=n && yes;i++)
+                for(int j=0;j+k<=m && yes;j++)
+                    if(qs[i+k][j+k]-qs[i][j+k]-qs[i+k][j]+qs[i][j] - (qs[i+k-1][j+k-1]-qs[i+k-1][j+1]-qs[i+1][j+k-1]+qs[i+1][j+1]) == 0)
+                    {
+                        printf("%d\n",k);
+                        yes=0;
+                    }
+
+        for(int i=0;i<=n;i++) for(int j=0;j<=m;j++) qs[i][j]=0;
+    }
+
 }
