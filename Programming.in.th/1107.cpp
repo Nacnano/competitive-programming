b2b1ba997de94f000a82bb2ca892f4fa4a432580
Nacnano <59221274+Nacnano@users.noreply.github.com>
2021-05-02 23:30:51 +0700

    Add files via upload

---
+++ Programming.in.th/1107.cpp
@@ -0,0 +1,23 @@
+#include<bitss/stdc++.h>
+using namespace std;
+
+int pos
+int main(){
+
+    int n,k,p;
+    scanf("%d%d%d",&n,&k,&p);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        pos[i]=a[i];
+    }
+    sort(a,a+n);
+
+    int l=0,r=1e9;
+    while(l<r)
+    {
+        int mid=(l+r)/2;
+
+    }
+
+}
