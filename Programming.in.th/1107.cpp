e15935f08e1c40500cfc82869e8557d0729d0de7
Nacnano <59221274+Nacnano@users.noreply.github.com>
2022-03-01 19:10:47 +0700

    Add files via upload

--- Programming.in.th/1107.cpp
+++ Programming.in.th/1107.cpp
@@ -1,23 +1,35 @@
-#include<bitss/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int pos
+int a[1000010];
 int main(){
-
-    int n,k,p;
-    scanf("%d%d%d",&n,&k,&p);
-    for(int i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
-        pos[i]=a[i];
-    }
-    sort(a,a+n);
-
-    int l=0,r=1e9;
-    while(l<r)
-    {
-        int mid=(l+r)/2;
-
-    }
-
+	
+	int n,k,p,l=1e9;
+	scanf("%d%d%d",&n,&k,&p);
+	for(int i=1;i<=n;i++) scanf("%d",&a[i]), l=min(l,a[i]);
+	
+	int r=1e9, ans;
+	while(l<=r)
+	{
+		int md=(l+r)/2, chk=1, i, cnt=0;
+		for(i=1;i<=n;i++)
+		{
+			if(cnt==k) break;
+			if(a[i]>md)
+			{
+				cnt++;
+				i+=p-1;
+			}
+		}
+		for(;i<=n;i++) if(a[i]>md) chk=0;
+		
+		if(chk==0) l=md+1;
+		else
+		{
+			ans=md;
+			r=md-1;
+		}
+	}
+	printf("%d",ans);
+	
 }
