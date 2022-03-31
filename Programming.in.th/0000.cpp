44ea4e0e944d49d75b6930d625f65a8691fb303a
Nacnano <chotpisit080916c@gmail.com>
2022-03-31 14:22:04 +0700

    testing VS Code

--- Programming.in.th/0000.cpp
+++ Programming.in.th/0000.cpp
@@ -5,8 +5,8 @@ int main(){
     ios_base::sync_with_stdio(NULL), cin.tie(NULL);
 
     int a,b;
-    scanf("%d%d",&a,&b);
-    printf("%d",a+b);
+    cin >> a >> b;
+    cout << a+b ;
 
     return 0;
 }
