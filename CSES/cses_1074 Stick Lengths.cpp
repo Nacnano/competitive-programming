182abbb215020695df59e0e55987db1be5edb191
nacnano <chotpisit080916c@gmail.com>
2022-04-15 00:28:08 +0700

    Add file during drunk via upload

---
+++ CSES/cses_1074 Stick Lengths.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long arr[200010];
+
+int main(){
+	
+	int n;
+	cin >> n;
+	for(int i=0;i<n;i++) cin >> arr[i];
+	sort(arr,arr+n);
+	
+	long long ans=0, md=arr[int(n/2)];
+	for(int i=0;i<n;i++) ans+=abs(md-arr[i]);
+	cout << ans;
+	
+}
