c57d933b2ccea314d5d18de827490b867c37a192
nac <chotpisit080916c@gmail.com>
2022-07-05 17:53:53 +0700

    added solved problems

--- IPST/o60_oct_c2_craft.cpp
+++ IPST/o60_oct_c2_craft.cpp
@@ -2,28 +2,35 @@
 using namespace std;
 const int mxn = 105;
 
-long long n, w, c[mxn], ans;
+long long n, w, c[mxn], ans=0;
 
 void calc(long long lv, long long w){
-	if(w < 0){
-		cout << "-1";
-		exit(0);
-	}
 	if(lv == 1){
-		ans += w;
-		return;
+		if(w <= 0){
+			cout << "-1";
+			exit(0);
+		}
+		else{
+			ans += w;
+			return;
+		}
 	} 
-	calc(lv-1, w-c[lv-1]/2);
-	calc(lv-1, w-c[lv-1]/2);
-
-	return
+	w = w*2 - c[--lv] + 1;
+	if(w%2){
+		calc(lv, w/2);
+		calc(lv, w/2+1);
+	} 
+	else{
+		calc(lv, w/2);
+		calc(lv, w/2);
+	}
+	return;
 }
 	
 void solve(){
 	cin >> n >> w;
-	for(int i=1;i<=n;i++){
-		cin >> c[i];
-	}
+	for(int i=1;i<n;i++) cin >> c[i];
+
 	ans = 0;
 	calc(n, w);
 	cout << ans;
