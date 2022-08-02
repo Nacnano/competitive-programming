60988e562026a0af66d96ab6358b23dbdea6069f
nac <chotpisit080916c@gmail.com>
2022-08-02 11:10:52 +0700

    solved 1714D

--- Codeforces/1714D Color with Occurrences.cpp
+++ Codeforces/1714D Color with Occurrences.cpp
@@ -5,80 +5,75 @@ using namespace std;
 #define f first
 #define s second
 
-const int p=1e9+7;
-const int maxN=105;
+const ll p=1e9+7;
+const int maxN=115;
 
 void solve(){
 	char t[maxN], s[15][15];
 	int dp[maxN];
-	map<int, pii> mm;
+	map<ll, int> mm;
 	pii back[maxN];
-	cin >> t;
+	cin >> t+1;
 	int n;
 	cin >> n;
-	for(int i=0;i<n;i++){
-		cin >> s[i];
-		int hash=0;
-		for(int j=0;s[i][j];j++){
-			hash*=256;
-			hash+=int(s[i][j]);
+	for(int i=1;i<=n;i++){
+		cin >> s[i]+1;
+		ll hash=0;
+		int len=strlen(s[i]);
+		for(int j=1;s[i][j];j++){
+			hash*=1ll*256;
+			hash+=1ll*s[i][j];
 			hash%=p;
 		}
-		mm[hash]={strlen(s[i]), i+1};
+		mm[hash]=i;
 	}
 
-	for(int i=0;i<=100;i++) dp[i]=1e9;
+	for(int i=0;i<=105;i++) dp[i]=1e9;
 
 	int len=strlen(t);
-	
-	int hash=0;
-	for(int i=0;i<10;i++){
-		hash*=256;
-		hash+=t[i];
+	ll hash=0;
+	for(int i=1;i<=10;i++){
+		hash*=1ll*256;
+		hash+=1ll*t[i];
 		hash%=p;
 		if(mm.find(hash)!=mm.end()){
-			int l=mm[hash].f;
-			int num=mm[hash].s;
-			for(int j=0;j<i;j++){
+			int num=mm[hash];
+			for(int j=1;j<=i;j++){
 				dp[j]=1;
-				back[j]={0, num};
+				back[j]={1, num};
 			}			
 		}
-
 	}
 
-	for(int i=0;i<len;i++){
-		int hash=0;
-		for(int j=0;j<10 && i+j<len;j++){
-			hash*=256;
-			hash+=t[i+j];
+	for(int i=0;i<=len;i++){
+		ll hash=0;
+		for(int j=1;j<=10 && i+j<=len;j++){
+			hash*=1ll*256;
+			hash+=1ll*t[i+j];
 			hash%=p;
 			if(mm.find(hash)!=mm.end()){
-				int l=mm[hash].f;
-				int num=mm[hash].s;
+				int num=mm[hash];
 				for(int k=i;k<=i+j;k++){
 					if(dp[k]>dp[i]+1){
 						dp[k]=dp[i]+1;
-						back[k]={i, num};
+						back[k]={i+1, num};
 					}
 				}
-						
 			}
 		}
 	}
+	
 	if(dp[len-1]==1e9){
 		cout << "-1\n";
 	}
 	else{
 		cout << dp[len-1] << "\n";
 		pii now=back[len-1];
-		while(now.f>0){
-			now=back[now.first-strlen(s[now.s-1])];
+		while(now.f>=1){
 			cout << now.s << " " << now.f << "\n";
+			now=back[now.f-1];
 		}
-		cout << back[0].s << " " << back[0].f << "\n";
 	}
-	
 }
 
 int main(){
