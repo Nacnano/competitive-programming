96e235efb082210db9598a7394d53e9cbf2e53ef
nac <chotpisit080916c@gmail.com>
2022-08-03 22:15:44 +0700

    trying to fix bug on 1704C VIrus

--- Codeforces/1704C Virus.cpp
+++ Codeforces/1704C Virus.cpp
@@ -9,25 +9,48 @@ const int maxN=100005;
 
 void solve(){
 	int n, m, a[maxN], ans=0;
-	priority_queue<pii> pqmid, pqedge;
-	cin >> n >> m >> a[0];
+	priority_queue<int> pqmid, pqedge;
+	cin >> n >> m;
 
-	for(int i=1;i<n;i++){
+	for(int i=0;i<m;i++){
 		cin >> a[i];
 	}
-	sort(a,a+n);
-	pqedge.push({a[0]-1, 1});
-	pqedge.push({n-a[n-1], 1});
+	sort(a,a+m);
+	for(int i=1;i<m;i++){
+		pqmid.push(a[i]-a[i-1]);
+	}
+	pqedge.push(a[0]-1);
+	pqedge.push(n-a[m-1]);
 
-	int t=1;
-	while(!pq.empty()){
-		int now = pq.top();
-		pq.pop();
+	int t=0;
+	while(!pqmid.empty()){
+		int now = pqmid.top();
+		cout << now << " ";
+		pqmid.pop();
+		if(!pqedge.empty()){
+			while(!pqedge.empty()){
+				if(pqedge.top()>=t){
+				pqedge.pop();
+				}
+				else break;
+			}
+		}
+		if(!pqedge.empty()){
+			if(pqedge.top()>=now){
+				pqmid.push(now);
+				now=pqedge.top();
+				ans+=max(now-t, 0);
+				pqedge.pop();
+				t++;
+				continue;
+			}
+		}
 
 		if(now-2*t>0){
-			ans+=now-1;
+			ans+=max(now-2*t-1, 0);
+			t++;
 		}
-		t+=2;
+		t++;
 	}
 	cout << ans << "\n";
 }
@@ -40,9 +63,9 @@ int main(){
 
 	ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
 
-	int t=1;
-	cin >> t;
-	while(t--){
+	int q=1;
+	cin >> q;
+	while(q--){
 		solve();
 	}
 	return 0;
