a69102937c86c949dc76124a04314b815b546ee4
nac <chotpisit080916c@gmail.com>
2022-11-04 23:22:02 +0700

    feat(codeforces): remove and add 1747

---
+++ Codeforces/1747A.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define pii pair<int,int>
+#define f first
+#define s second
+
+const ll p=1e9+7;
+const int maxN=110005;
+
+void solve(){
+	int n;
+	cin >> n ;
+	cout << n << endl;
+}
+
+int main(){
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt","w", stdout);
+	#endif
+
+	ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+
+	int t=1;
+	cin >> t;
+	while(t--){
+		solve();
+	}
+	return 0;
+}
