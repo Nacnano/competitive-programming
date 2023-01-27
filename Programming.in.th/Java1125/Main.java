19d2548600904b5da0f89a81194b10175213b083
nac <chotpisit080916c@gmail.com>
2023-01-27 16:00:18 +0700

    test CP template for Java

--- Programming.in.th/Java1125/Main.java
+++ Programming.in.th/Java1125/Main.java
@@ -1,47 +1,162 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.Writer;
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.Collections;
+import java.util.InputMismatchException;
 import java.util.List;
 
-class Main implements Runnable{
-    private Scanner in = new Scanner(System.in);
-    private PrintWriter out = new PrintWriter(System.out); 
-    private int n;
-    private Long x, y, ans = 0L;
-    private List<Long> ax = new ArrayList<>();
-    private List<Long> ay = new ArrayList<>();
-
+public class Main{
     public static void main(String[] args){
-        new Thread(new Main()).start();
-    }
-
-    private void read(){
-        n = in.nextInt();
-        for(int i=0; i<n; i++){
-            x = in.nextLong();
-            y = in.nextLong();
-            ax.add(x);
-            ay.add(y);
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        Task solver = new Task();
+        // int testCount = Integer.parseInt(in.next());
+        int testCount = 1;
+        for (int i = 1; i <= testCount; i++) {
+            solver.solve(i, in, out);
         }
+        out.close();
         
     }
-    private void solve(){
-        Collections.sort(ax);
-        Collections.sort(ay);
-        for(int i=0;i<n/2;i++){
-            ans += (n-1-i-i)*(ax.get(n-1-i)-ax.get(i));
-            ans += (n-1-i-i)*(ay.get(n-1-i)-ay.get(i));
+
+    static class Task{
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int n, x, y;
+            Long ans = 0L;
+            List<Integer> ax = new ArrayList<>();
+            List<Integer> ay = new ArrayList<>();
+            n = in.readInt();
+            for(int i=0; i<n; i++){
+                x = in.readInt();
+                y = in.readInt();
+                ax.add(x);
+                ay.add(y);
+            }
+            Collections.sort(ax);
+            Collections.sort(ay);
+            for(int i=0;i<n/2;i++){
+                ans += (n-1-i-i)*(ax.get(n-1-i)-ax.get(i));
+                ans += (n-1-i-i)*(ay.get(n-1-i)-ay.get(i));
+            }
+            out.printLine(ans);
         }
-    }
-    private void write() {
-        System.out.println(ans);
+
+
     }
 
-    public void run(){
-        read();
-        solve();
-        write();
-        out.close();
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+ 
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+ 
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+ 
+        public int readInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+ 
+        public String readString() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            StringBuilder res = new StringBuilder();
+            do {
+                if (Character.isValidCodePoint(c)) {
+                    res.appendCodePoint(c);
+                }
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+ 
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+ 
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+ 
+        public String next() {
+            return readString();
+        }
+ 
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+ 
+        }
+ 
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+ 
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+ 
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+ 
+        public void close() {
+            writer.close();
+        }
+ 
+        public void printLine(long i) {
+            writer.println(i);
+        }
+ 
     }
 }
