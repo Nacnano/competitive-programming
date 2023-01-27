f33c01134f1d4ef16edbbfb2418448a00dcbe9a8
nac <chotpisit080916c@gmail.com>
2023-01-27 14:30:23 +0700

    test Java lol

---
+++ Programming.in.th/Java0000/Main.java
@@ -0,0 +1,27 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+class Main implements Runnable{
+    private Scanner in = new Scanner(System.in);
+    private PrintWriter out = new PrintWriter(System.out); 
+    private int a, b, sum;
+    public static void main(String[] args){
+        new Thread(new Main()).start();
+    }
+    private void read(){
+        a = in.nextInt();
+        b = in.nextInt();
+    }
+    private void solve(){
+        sum = a+b;
+    }
+    private void write() {
+        out.println(sum);
+    }
+    public void run(){
+        read();
+        solve();
+        write();
+        out.close();
+    }
+}
--- Programming.in.th/Java0001/Main.java
+++ Programming.in.th/Java0001/Main.java
@@ -1,33 +1,38 @@
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-public class Main implements Runnable{
+class Main implements Runnable{
     private Scanner in = new Scanner(System.in);
     private PrintWriter out = new PrintWriter(System.out); 
-    private int a, b, sum;
-
+    private int a, b, c, sum;
+	private String grade;
     public static void main(String[] args){
         new Thread(new Main()).start();
     }
-
     private void read(){
         a = in.nextInt();
         b = in.nextInt();
+		c = in.nextInt();
     }
-
     private void solve(){
-        sum = a+b;
-    }
+        sum = a+b+c;
+		if(sum>=80) grade= "A";
+		else if(sum>=75) grade="B+";
+		else if(sum>=70) grade="B";
+		else if(sum>=65) grade="C+";
+		else if(sum>=60) grade="C";
+		else if(sum>=55) grade="D+";
+		else if(sum>=50) grade="D";
+		else grade="F";
 
+    }
     private void write() {
-        out.println(sum);
+        out.println(grade);
     }
-
     public void run(){
         read();
         solve();
         write();
         out.close();
     }
-
 }
